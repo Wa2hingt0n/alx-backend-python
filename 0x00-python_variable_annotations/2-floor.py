49d361b19586f876e2ff1c3433f164cd053122aa
Washington Obungu <tobungu@gmail.com>
2022-06-23 08:25:48 +0000

    Added a function that returns the floor of an number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#/usr/bin/env python3
+""" Defines a type-annotated function returns the floor of a float """
+from math import floor as fl
+
+
+def floor(n: float) -> int:
+    """ Returns the floor of a float as an integer
+
+    Args:
+        n (float): The number to be returned as a floor
+    """
+    return fl(n)
