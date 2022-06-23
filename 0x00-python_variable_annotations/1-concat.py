65a51cf40c2bbbeb27c6c8ec26a5453dd1a88bc4
Washington Obungu <tobungu@gmail.com>
2022-06-23 07:58:57 +0000

    Added a function that concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module defines a type annotated function """
+def concat(str1: str, str2: str) -> str:
+    """ Returns a concatenated string
+
+    Args:
+        str1 (string): First dtring
+        str2 (string): Second string
+    """
+    return str1 + str2
