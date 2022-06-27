82827e2571fffa2753564f31d162ef626e9c9e1d
Washington Obungu <tobungu@gmail.com>
2022-06-27 12:59:17 +0000

    Added the gather methos

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,9 +9,5 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Returns a list of delays after spawning wait_random n times """
-    delay_list: List[float] = []
-    for i in range(0, n):
-        #delay: float = await wait_random(max_delay)
-        delay_list.append(await (wait_random(max_delay)))
-        i += 1
-    return delay_list
+    delay = await asyncio.gather(*(wait_random(max_delay)for i in range(n)))
+    return sorted(delay)
