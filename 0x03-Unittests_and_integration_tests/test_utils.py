8c49446fbb5de91edc4b7f58216fd70b982d0aa2
Washington Obungu <tobungu@gmail.com>
2022-07-18 17:37:13 +0000

    Added a test suite for the utils.memoize method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ import unittest
 from parameterized import parameterized
 from typing import Dict, Tuple, Any, Union
 from unittest.mock import Mock, patch
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -49,3 +49,24 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**kwargs)) as mock_req:
             self.assertEqual(get_json(test_url), test_payload)
             mock_req.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Creates a test suite for the utils.memoize method """
+    def test_memoize(self):
+        """ Tests the utils.memoize method """
+        class TestClass:
+            """ Creates a test class """
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(
+                TestClass, "a_method", return_value=lambda: 42) as mock_memo:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            mock_memo.assert_called_once()
