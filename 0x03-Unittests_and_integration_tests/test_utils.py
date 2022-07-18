e319344e3ca9479b59d54d933b7ea0b72c7e098a
Washington Obungu <tobungu@gmail.com>
2022-07-18 08:40:50 +0000

    Added test case

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Defines a test class 'TestAccessNestaedMap' """
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Creates a parameterized test for the utils.access_nested_map method """
+    @parameterized.expand([
+        ("not_nested", {"a": 1}, ("a",), 1),
+        ("nested_idx_0", {"a": {"b": 2}}, ("a",), {"b": 2}),
+        ("nested_value", {"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, name, nested_map, path,  expected):
+        """ Tests the 'utils.access_nested_map method' """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
