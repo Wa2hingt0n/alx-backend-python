e0e5c14af64fd49c32a4c009e29784ba4f6d9520
Washington Obungu <tobungu@gmail.com>
2022-07-18 08:53:59 +0000

    Added type annotations

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Defines a test class 'TestAccessNestaedMap' """
 import unittest
+from typing import Dict, Tuple, Any
 from parameterized import parameterized
 from utils import access_nested_map
 
@@ -12,6 +13,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ("nested_idx_0", {"a": {"b": 2}}, ("a",), {"b": 2}),
         ("nested_value", {"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, name, nested_map, path,  expected):
+    def test_access_nested_map(
+            self,
+            name: str, nested_map: Dict[Any],
+            path: Tuple[str, ...],
+            expected: Any) -> Any:
         """ Tests the 'utils.access_nested_map method' """
         self.assertEqual(access_nested_map(nested_map, path), expected)
